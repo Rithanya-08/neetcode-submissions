b62628ac64f750136d336c0e2303735d51b50557
Rithanya-08 <147416739+Rithanya-08@users.noreply.github.com>
2026-09-15 21:43:03 +0530

    Add: min-cost-climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-1.py
@@ -0,0 +1,10 @@
+from functools import lru_cache
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        @lru_cache(maxsize = None)
+        def solve(i):
+            if(i>=len(cost)):
+                return 0
+            return cost[i] + min(solve(i+1),solve(i+2))
+
+        return min(solve(0),solve(1))
